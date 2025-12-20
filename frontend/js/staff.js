504b10c1af500e7507feee7eca74d0047280eec7
LeAnhHuy2409 <huyleanh2409@gmail.com>
2025-12-20 20:31:13 +0700

    dự án hoàn thành

--- frontend/js/staff.js
+++ frontend/js/staff.js
@@ -179,7 +179,12 @@ document.addEventListener("DOMContentLoaded", () => {
                 const statusSelect = `
                     <select onchange="updateStatus(${order.maDonHang}, this.value)" 
                             class="status-select status-${getStatusClass(order.trangThai)}"
-                            ${order.trangThai === 'Đã hủy' ? 'disabled' : ''}>
+                            ${
+                              order.trangThai === "Đã hủy" ||
+                              order.trangThai === "Hoàn thành"
+                                ? "disabled"
+                                : ""
+                            }>
                         <option value="Chờ xác nhận" ${order.trangThai === 'Chờ xác nhận' ? 'selected' : ''}>Chờ xác nhận</option>
                         <option value="Đang xử lý" ${order.trangThai === 'Đang xử lý' ? 'selected' : ''}>Đang xử lý</option>
                         <option value="Đang giao" ${order.trangThai === 'Đang giao' ? 'selected' : ''}>Đang giao</option>
